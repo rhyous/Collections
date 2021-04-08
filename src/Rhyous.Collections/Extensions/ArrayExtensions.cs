a565396770b2f9ab7907d859ffd6e6d3b6dd9bd3
Jared Barneck <rhyous@yahoo.com>
2021-04-07 18:23:13 -0600

    Add method to Fill multidimentional Array

---
+++ src/Rhyous.Collections/Extensions/ArrayExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Rhyous.Collections.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static T[] Fill<T>(this T[] array, T value)
+        {
+            if (array is null) { throw new ArgumentNullException(nameof(array)); }
+            for (int i = 0; i < array.Length; i++)
+                array[i] = value;
+            return array;
+        }
+
+        public static T[,] Fill<T>(this T[,] array, T value)
+        {
+            if (array is null) { throw new ArgumentNullException(nameof(array)); }
+            var jLength = array.GetLength(0);
+            var kLength = array.GetLength(1);
+            for (int i = 0; i < array.Length; i++)
+                for (int j = 0; j < jLength; j++)
+                    for (int k = 0; k < kLength; k++)
+                        array[j, k] = value;
+            return array;
+        }
+
+        public static T[,,] Fill<T>(this T[,,] array, T value)
+        {
+            if (array is null) { throw new ArgumentNullException(nameof(array)); }
+            var jLength = array.GetLength(0);
+            var kLength = array.GetLength(1);
+            var lLength = array.GetLength(2);
+            for (int i = 0; i < array.Length; i++)
+                for (int j = 0; j < jLength; j++)
+                    for (int k = 0; k < kLength; k++)
+                        for (int l = 0; l < lLength; l++)
+                            array[j, k, l] = value;
+            return array;
+        }
+
+        public static T[,,,] Fill<T>(this T[,,,] array, T value)
+        {
+            if (array is null) { throw new ArgumentNullException(nameof(array)); }
+            var jLength = array.GetLength(0);
+            var kLength = array.GetLength(1);
+            var lLength = array.GetLength(2);
+            var mLength = array.GetLength(3);
+            for (int i = 0; i < array.Length; i++)
+                for (int j = 0; j < jLength; j++)
+                    for (int k = 0; k < kLength; k++)
+                        for (int l = 0; l < lLength; l++)
+                            for (int m = 0; m < mLength; m++)
+                                array[j, k, l, m] = value;
+            return array;
+        }
+
+        public static T[,,,,] Fill<T>(this T[,,,,] array, T value)
+        {
+            if (array is null) { throw new ArgumentNullException(nameof(array)); }
+            var jLength = array.GetLength(0);
+            var kLength = array.GetLength(1);
+            var lLength = array.GetLength(2);
+            var mLength = array.GetLength(3);
+            var nLength = array.GetLength(4);
+            for (int i = 0; i < array.Length; i++)
+                for (int j = 0; j < jLength; j++)
+                    for (int k = 0; k < kLength; k++)
+                        for (int l = 0; l < lLength; l++)
+                            for (int m = 0; m < mLength; m++)
+                                for (int n = 0; n < nLength; n++)
+                                    array[j, k, l, m, n] = value;
+            return array;
+        }
+    }
+}
