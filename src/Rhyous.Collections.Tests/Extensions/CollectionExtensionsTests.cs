cfbeac6bba7b075a462fc4f32aa40d0890f5acae
Jared Barneck <rhyous@yahoo.com>
2022-01-28 13:00:13 -0700

    Added a CollectionExtensions with LastIndex() that provides the last index of an array, which just wraps Count -1 but the method name adds clarity and simplicity.

---
+++ src/Rhyous.Collections.Tests/Extensions/CollectionExtensionsTests.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Rhyous.Collections.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace Rhyous.Collections.Tests.Extensions
+{
+    [TestClass]
+    public class CollectionExtensionsTests
+    {
+        #region LastIndex
+        [TestMethod]
+        public void CollectionExtensions_LastIndex_Collection_Null_ThrowIfNull_Default_Throws()
+        {
+            // Arrange
+            List<object> collection = null;
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                collection.LastIndex();
+            });
+        }
+
+        [TestMethod]
+        public void CollectionExtensions_LastIndex_Collection_Null_ThrowIfNull_True_Throws()
+        {
+            // Arrange
+            List<object> collection = null;
+            bool throwIfNull = true;
+
+            // Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(() =>
+            {
+                collection.LastIndex(throwIfNull);
+            });
+        }
+
+        [TestMethod]
+        public void CollectionExtensions_LastIndex_Collection_Null_ThrowIfNull_False_Returns_Minus1()
+        {
+            // Arrange
+            List<object> collection = null;
+            bool throwIfNull = false;
+
+            // Act & Assert
+            var result = collection.LastIndex(throwIfNull);
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void CollectionExtensions_LastIndex_Collection_Empty_Returns_Minus1()
+        {
+            // Arrange
+            var collection = new List<object>();
+
+            // Act & Assert
+            var result = collection.LastIndex();
+
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void CollectionExtensions_LastIndex_Collection_Populated_Returns_CountMinus1()
+        {
+            // Arrange
+            var collection = new List<object> { 1, 2, 3, "a", "b", "c" };
+
+            // Act & Assert
+            var result = collection.LastIndex();
+
+            // Assert
+            Assert.AreEqual(collection.Count - 1, result);
+        }
+        #endregion
+    }
+}
---
+++ src/Rhyous.Collections/Extensions/CollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace Rhyous.Collections
+{
+    /// <summary>Extension methods for ICollection.</summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>Returns the last index of a list.</summary>
+        /// <param name="collection">The ICollection instance or object that inherits ICollection, such as any array or list.</param>
+        /// <param name="throwIfNull">True by default. Whether to throw an ArgumentNullException if the collection is null or whether to return -1.</param>
+        /// <returns>The last index of the collection or -1 if the collection is empty, or if it is null and throwIfNull is false.</returns>
+        /// <exception cref="ArgumentNullException">An ArgumentNullException will be thrown if throwIfNull is true and the collection is null.</exception>
+        public static int LastIndex(this ICollection collection, bool throwIfNull = true)
+        {
+            if (throwIfNull && collection is null) { throw new ArgumentNullException(nameof(collection)); };
+            return collection?.Count - 1 ?? -1;
+        }
+    }
+}
