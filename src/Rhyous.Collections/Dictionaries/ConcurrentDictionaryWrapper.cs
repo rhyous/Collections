fe5e25a3c60a7d69fecc7ccdbcf641c4e52fd391
Jared Barneck <rhyous@yahoo.com>
2022-02-15 10:31:10 -0700

    Added ConcurrentDictionaryWrapper and an IConcurrentDictionary interface.

---
+++ src/Rhyous.Collections/Dictionaries/ConcurrentDictionaryWrapper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Rhyous.Collections
+{
+    /// <inheritdoc/>
+    public class ConcurrentDictionaryWrapper<TKey, TValue> : ConcurrentDictionary<TKey, TValue>, IConcurrentDictionary<TKey, TValue>
+    {
+        #region Constructors
+        /// <inheritdoc/>
+        public ConcurrentDictionaryWrapper()
+        {
+        }
+
+        /// <inheritdoc/>
+        public ConcurrentDictionaryWrapper(IEnumerable<KeyValuePair<TKey, TValue>> collection) : base(collection)
+        {
+        }
+
+        /// <inheritdoc/>
+        public ConcurrentDictionaryWrapper(IEqualityComparer<TKey> comparer) : base(comparer)
+        {
+        }
+
+        /// <inheritdoc/>
+        public ConcurrentDictionaryWrapper(int concurrencyLevel, int capacity) : base(concurrencyLevel, capacity)
+        {
+        }
+
+        /// <inheritdoc/>
+        public ConcurrentDictionaryWrapper(IEnumerable<KeyValuePair<TKey, TValue>> collection, IEqualityComparer<TKey> comparer) : base(collection, comparer)
+        {
+        }
+
+        /// <inheritdoc/>
+        public ConcurrentDictionaryWrapper(int concurrencyLevel, IEnumerable<KeyValuePair<TKey, TValue>> collection, IEqualityComparer<TKey> comparer) : base(concurrencyLevel, collection, comparer)
+        {
+        }
+
+        /// <inheritdoc/>
+        public ConcurrentDictionaryWrapper(int concurrencyLevel, int capacity, IEqualityComparer<TKey> comparer) : base(concurrencyLevel, capacity, comparer)
+        {
+        }
+        #endregion
+    }
+}
---
+++ src/Rhyous.Collections/Dictionaries/IConcurrentDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Rhyous.Collections
+{
+    /// <summary>An interface for ConcurrentDictionary</summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    /// <remarks>As Microsoft did not give ConcurrentDictionary an interface, this does, but ConcurrentDictionary doesn't implement it; instead,
+    /// use ConcurrentDictionaryWrapper.
+    /// </remarks>
+    public interface IConcurrentDictionary<TKey, TValue> : IDictionary<TKey, TValue>, ICollection<KeyValuePair<TKey, TValue>>, IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable, IDictionary, ICollection, IReadOnlyDictionary<TKey, TValue>, IReadOnlyCollection<KeyValuePair<TKey, TValue>>
+    {
+        #region Concurrent Dictionary
+        /// <inheritdoc/>
+        bool IsEmpty { get; }
+        /// <inheritdoc/>
+        TValue AddOrUpdate(TKey key, Func<TKey, TValue> addValueFactory, Func<TKey, TValue, TValue> updateValueFactory);
+        /// <inheritdoc/>
+        TValue AddOrUpdate(TKey key, TValue addValue, Func<TKey, TValue, TValue> updateValueFactory);
+        /// <inheritdoc/>
+        TValue GetOrAdd(TKey key, Func<TKey, TValue> valueFactory);
+        /// <inheritdoc/>
+        TValue GetOrAdd(TKey key, TValue value);
+        /// <inheritdoc/>
+        KeyValuePair<TKey, TValue>[] ToArray();
+        /// <inheritdoc/>
+        bool TryAdd(TKey key, TValue value);
+        /// <inheritdoc/>
+        bool TryRemove(TKey key, out TValue value);
+        /// <inheritdoc/>
+        bool TryUpdate(TKey key, TValue newValue, TValue comparisonValue);
+        #endregion
+    }
+}
