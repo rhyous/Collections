5309b56c06e3a36d648b64cd0ee2c00c50f9655f
Jared Barneck <rhyous@yahoo.com>
2022-02-28 14:46:45 -0700

    Added a common CacheBase object

---
+++ src/Rhyous.Collections/Cache/CacheBase.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+
+namespace Rhyous.Collections
+{
+    /// <summary>A base class for caching data.</summary>
+    /// <typeparam name="T">The type of collection. Must be IClearable and ICountable.</typeparam>
+    public abstract class CacheBase<T> : ICacheBase<T> where T : IClearable, ICountable
+    {
+        /// <summary>The underlying cache backing field</summary>
+        protected readonly T _Cache;
+
+        /// <summary>The Cache constructor. Must take in an instance of the cache collection.</summary>
+        /// <param name="cache"></param>
+        public CacheBase(T cache)
+        {
+            _Cache = cache;
+        }
+
+        /// <summary>Clears the cache. This will cause it to reload on next request.</summary>
+        public virtual void Clear()
+        {
+            _Cache.Clear();
+        }
+
+        /// <summary>Provides the cache. Creates the cache if not created or empty.</summary>
+        /// <param name="forceUpdate">Default = false. Updates the cache if true.</param>
+        /// <returns>The cache.</returns>
+        public virtual async Task<T> ProvideAsync(bool forceUpdate = false)
+        {
+            if (forceUpdate)
+                Clear();
+            if (_Cache.Count == 0)
+                await CreateCacheAsync();
+            return _Cache;
+        }
+
+        /// <summary>This creates the cache. This should only be called one time
+        /// or whenever and update is forced.</summary>
+        /// <returns></returns>
+        protected abstract Task CreateCacheAsync();
+    }
+}
---
+++ src/Rhyous.Collections/Interfaces/ICacheBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace Rhyous.Collections
+{
+    /// <summary>A base class for caching data.</summary>
+    /// <typeparam name="T">The type of collection. Must be IClearable and ICountable.</typeparam>
+    public interface ICacheBase<T> where T : IClearable, ICountable
+    {
+        /// <summary>Clears the cache. This will cause it to reload on next request.</summary>
+        void Clear();
+        /// <summary>Provides the cache. Creates the cache if not created or empty.</summary>
+        /// <param name="forceUpdate">Default = false. Updates the cache if true.</param>
+        /// <returns>The cache.</returns>
+        Task<T> ProvideAsync(bool forceUpdate = false);
+    }
+}
